ff90a823efa7723ffc7f85123c398c4a7b7d5f0f
CallanGeorge <callangeorge19@gmail.com>
2022-10-29 14:06:17 +0100

    Added Product component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,15 @@ import { Container } from 'react-bootstrap'
 import Header from './components/header'
 import Footer from './components/footer'
 
+import HomeScreen from './screens/homescreen'
+
 function App() {
   return (
     <div>
       <Header />
         <main className='py-3'>
           <Container>
-            <h1>My app</h1>
+            <HomeScreen />
           </Container>
         </main>
       <Footer />
---
+++ frontend/src/products.js
@@ -0,0 +1,44 @@
+const products =[
+    {
+        '_id': '1',
+        'name':'Polo Hoodie',
+        'image':'/images/polohoodie.jpeg',
+        'description':
+            'Ralph Lauren Polo Hoodie',
+        'brand':'Ralph Lauren POLO',
+        'category':'clothing',
+        'price': 89.99,
+        'countInStock': 10,
+        'rating': 5,
+        'numReviews': 1,
+    },
+    {
+        '_id': '2',
+        'name':'Polo Shirt',
+        'image':'/images/poloshirt.jpeg',
+        'description':
+            'Ralph Lauren Polo Shirt',
+        'brand':'Ralph Lauren POLO',
+        'category':'clothing',
+        'price': 69.99,
+        'countInStock': 10,
+        'rating': 5,
+        'numReviews': 1,
+    },
+    {
+        '_id': '1',
+        'name':'Polo Sweat Pants',
+        'image':'/images/polosweatpants.jpeg',
+        'description':
+            'Ralph Lauren Polo Sweat Pants',
+        'brand':'Ralph Lauren POLO',
+        'category':'clothing',
+        'price': 59.99,
+        'countInStock': 10,
+        'rating': 5,
+        'numReviews': 1,
+    },
+
+]
+
+export default products
